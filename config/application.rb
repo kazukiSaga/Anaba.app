f789f835b6cfe70a6883a266c4add62879b034af
kazukiSaga <kazukikk1351@gmail.com>
2024-09-03 00:25:12 +0900

    初期設定

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,8 @@ module Myapp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
