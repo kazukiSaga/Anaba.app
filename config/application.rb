23d2de8845ae325079512e581c466e3e0342e729
kazukiSaga <kazukikk1351@gmail.com>
2024-09-03 22:41:29 +0900

    不要なファイルとアプリケーション設定を更新

--- config/application.rb
+++ config/application.rb
@@ -39,5 +39,6 @@ module Myapp
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.hosts << 'https://anaba.onrender.com'
   end
 end
