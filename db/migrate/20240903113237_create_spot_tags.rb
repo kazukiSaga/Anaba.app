cd46a401729e64f113f0d8a552ecd0cf56848df8
kazukiSaga <kazukikk1351@gmail.com>
2024-09-04 13:06:53 +0900

    spot_tagテーブルカラム変更

--- db/migrate/20240903113237_create_spot_tags.rb
+++ db/migrate/20240903113237_create_spot_tags.rb
@@ -1,7 +1,7 @@
 class CreateSpotTags < ActiveRecord::Migration[6.1]
   def change
     create_table :spot_tags do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :spot, null: false, foreign_key: true
       t.references :tag, null: false, foreign_key: true
 
       t.timestamps
