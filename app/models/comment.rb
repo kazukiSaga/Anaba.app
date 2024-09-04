80477f01586dc5ea095ea075d1f1ebeecf7b4d64
kazukiSaga <kazukikk1351@gmail.com>
2024-09-04 13:35:37 +0900

    モデル修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :spot
-  has_many :memorys
+  has_many :memories
 
   validates :title, presence: true, length: { maximum: 13 }
   validates :body, presence: true, length: { maximum: 140 }
--- app/models/memory.rb
+++ app/models/memory.rb
@@ -1,24 +1,3 @@
 class Memory < ApplicationRecord
   belongs_to :comment
-  has_many_attached :avatars
-
-  validate :validate_avatars
-
-  private
-
-  def validate_avatars
-    if avatars.length > 4
-      errors.add(:avatars, "は4枚以内にしてください")
-    end
-
-    avatars.each do |avatar|
-      unless avatar.blob.content_type.in?(%w(image/jpeg image/png))
-        errors.add(:avatars, 'はjpegまたはpng形式でアップロードしてください')
-      end
-
-      if avatar.blob.byte_size > 5.megabytes
-        errors.add(:avatars, "は1つのファイル5MB以内にしてください")
-      end
-    end
-  end
 end
