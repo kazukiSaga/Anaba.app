966cf241faf0396db05bc897128a5567aa036d8e
kazukiSaga <kazukikk1351@gmail.com>
2024-09-05 20:27:54 +0900

    Vue.jsからRailsへの通信テスト用のAPIを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
+  # 以下を追加
+  def test
+    render json: { message: 'This is a test message from Rails' }
+  end
 end
