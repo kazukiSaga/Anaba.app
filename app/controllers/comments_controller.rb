0d0f67887b9820f727f5800da4231a471dd06790
kazukiSaga <kazukikk1351@gmail.com>
2024-09-07 12:22:34 +0900

    各コントローラー作成

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,16 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(:user_id, :spot_id, :title, :body, memories_attributes: [:image])
   end
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,22 @@
 class UsersController < ApplicationController
-def index
-end
+  def index
+  end
 
-def new
-end
+  def new
+  end
 
-def create
-end
+  def create
+  end
 
-def edit
-end
+  def edit
+  end
 
-def show
-end
+  def show
+  end
 
-def update
-end
+  def update
+  end
 
-def destroy
+  def destroy
+  end
 end
