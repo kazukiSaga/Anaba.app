e660fc38fbca2f82e3d27eaa0776820dcdc93860
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 04:23:47 +0900

    render.shを追加

---
+++ bin/render-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake db:migrate
+
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+bundle exec rake db:seed 
