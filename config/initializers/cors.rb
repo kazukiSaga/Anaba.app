f1f6dd0c65ea2f3a442e7037a24f2e74fc2f0bcb
kazukiSaga <kazukikk1351@gmail.com>
2024-09-17 05:00:28 +0900

    update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173' || "https://anaba-front.vercel.app"
+    origins 'localhost:5173', 'https://anaba-front.vercel.app'
 
     resource '*',
       headers: :any,
--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2024_09_03_113732) do
   create_table "spots", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "prefecture_id", null: false
-    t.strpgeming "name", null: false
+    t.string "name", null: false
     t.string "body", null: false
     t.string "city", null: false
     t.integer "average_rating"
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 2024_09_03_113732) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["email"], name: "index_users_on_email"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
