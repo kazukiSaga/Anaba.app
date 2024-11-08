84b3c758879e0b8560b5924ba321672fa34933da
kazukiSaga <kazukikk1351@gmail.com>
2024-11-08 17:34:59 +0900

    spotに紐づいたtagを取得できるよう設定

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -5,30 +5,49 @@ class Api::V1::SpotsController < ApplicationController
   def index
     page = params[:page] || 1
     prefectures = Prefecture.all
-    spots = Spot.all.page(page).per(10)
-    pagination = resources_with_pagination(spots)
+    tags = Tag.all # タグデータを取得
+    # spots = Spot.all.page(page).per(10)
+    spots = Spot.includes(:tags).page(page).per(10) # タグ情報を一緒に取得
 
+    # binding.pry
+
+    pagination = resources_with_pagination(spots)
     render json: {
-      spots: spots.as_json,
+      # spots: spots.as_json,
+      spots: spots.as_json(include: { tags: { only: [:id, :name] } }), # タグ情報を含める
       prefectures: prefectures.as_json(only: %i[id name]),
+      tags: tags.as_json(only: %i[id name]),
       pagination: pagination.as_json
     }, status: :ok
   end
 
   def new
-    prefectures = Prefecture.all
+      prefectures = Prefecture.all
+      tags = Tag.all
 
-    render json: {
-      prefectures: prefectures.as_json(only: %i[id name])
-    }, status: :ok
+
+      render json: {
+        prefectures: prefectures.as_json(only: %i[id name]),
+        tags: tags.as_json(only: %i[id name])
+      }, status: :ok
   end
 
   def create
     spot = Spot.new(spot_params)
     
+    if params[:tags].present?
+
+      tags = params[:tags].map do |tag_name|
+        Tag.find_or_create_by(name: tag_name.strip) # タグが存在しない場合は作成
+      end
+      spot.tags = tags
+    end
+
     if spot.save
+    #  タグ作成
+    # tag_spotの作成
       render json: {
-        spot: spot.as_json
+        spot: spot.as_json(include: { tags: { only: [:id, :name] } })
       }
     else
       render json: {
@@ -62,10 +81,13 @@ class Api::V1::SpotsController < ApplicationController
         include: {
           prefecture: {
             only: [:name]
+          },
+          tags: {
+            only: [:id, :name]
           }
         }
-      )
-    },status: :ok
+      ), # スポットに紐づく都道府県とタグ情報を含める
+    }, status: :ok
   end
 
   def update
@@ -83,6 +105,6 @@ class Api::V1::SpotsController < ApplicationController
   private
 
   def spot_params
-    params.permit(:name, :body, :prefecture_id, :city).merge(user_id: current_api_v1_user.id)
+    params.permit(:name, :body, :prefecture_id, :city, :longitude, :latitude).merge(user_id: current_api_v1_user.id)
   end
 end
