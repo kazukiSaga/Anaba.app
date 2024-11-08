a6f8eb32a82163810b59ab5ba845f64e33c039d7
kazukiSaga <kazukikk1351@gmail.com>
2024-11-08 17:05:27 +0900

    ransackで検索可能なテーブル、カラムを指定

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,6 +1,7 @@
-class Spot < ApplicationRecord
+class Spot < ApplicationRecord  
   belongs_to :user
   belongs_to :prefecture
+  has_many :spot_tags
   has_many :tags, through: :spot_tags
   has_many :assessments
   has_many :comments
@@ -14,7 +15,13 @@ class Spot < ApplicationRecord
 
   accepts_nested_attributes_for :assessments, allow_destroy: true
 
+   # 検索可能な属性を指定
   def self.ransackable_attributes(auth_object = nil)
-    ["name","prefecture_id"]
+    ["average_quiet_rating", "average_rating", "body", "city", "created_at", "id", "latitude", "longitude", "name", "prefecture_id", "updated_at", "user_id","created_at", "id", "spot_id", "tag_id", "updated_at"]
+  end
+
+  # 検索可能な関連を指定
+  def self.ransackable_associations(auth_object = nil)
+    ["assessments", "comments", "prefecture", "spot_tags", "tags", "user"]
   end
 end
