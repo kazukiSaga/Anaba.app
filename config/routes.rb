da5ada83f62e00860477a88e6e434dfa1394597b
kazukiSaga <kazukikk1351@gmail.com>
2024-11-12 00:57:44 +0900

    userコントローラーへのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,15 @@ Rails.application.routes.draw do
 
       resources :spots, only: [:index, :new, :edit,:create, :update, :show]  do
         resources :assessments,only: [:create]
+        resources :comments,only: [:index, :new, :show, :create]
       end
       resources :search_spots, only: [:index]
+      resources :marker_spots, only: [:index]
     end
   end
+      scope '/api/v1' do
+    resources :users, only: [:update]
+  end
+
   get '/api/test', to: 'application#test'
 end
