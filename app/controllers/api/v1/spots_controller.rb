156af86a3fd60cdbb0065fa29d40340e63845790
kazukiSaga <kazukikk1351@gmail.com>
2024-11-26 16:10:59 +0900

    タグ編集機能

--- app/controllers/api/v1/spots_controller.rb
+++ app/controllers/api/v1/spots_controller.rb
@@ -51,21 +51,41 @@ class Api::V1::SpotsController < ApplicationController
     end
   end
 
+  # def edit
+  #   spot = Spot.find(params[:id])
+  #   prefectures = Prefecture.all
+
+
+  #   render json: {
+  #     spot: spot.as_json(
+  #       include: {
+  #         prefecture: {
+  #           only: [:name]
+  #         }
+  #       }
+  #     ),
+  #     prefectures: prefectures.as_json(only: %i[id name]),
+  #   },status: :ok
+  # end
   def edit
     spot = Spot.find(params[:id])
     prefectures = Prefecture.all
-
+    tags = Tag.all # 全てのタグを取得 (または spot.tags で関連付けられたタグのみを取得)
 
     render json: {
       spot: spot.as_json(
         include: {
           prefecture: {
             only: [:name]
+          },
+          tags: { 
+            only: [:id, :name]
           }
         }
       ),
       prefectures: prefectures.as_json(only: %i[id name]),
-    },status: :ok
+      tags: tags.as_json(only: %i[id name])
+    }, status: :ok
   end
 
   def show
@@ -85,16 +105,34 @@ class Api::V1::SpotsController < ApplicationController
     }, status: :ok
   end
 
+  # def update
+  #   spot = Spot.find(params[:id])
+  #   if spot.update(spot_params)
+  #     render json: {},status: :created
+  #   else
+  #     render json: {
+  #       errors: spot.errors.full_messages
+  #     },status: :bad_request
+  #   end
+    
+  # end
+
   def update
     spot = Spot.find(params[:id])
-    if spot.update(spot_params)
-      render json: {},status: :created
-    else
-      render json: {
-        errors: spot.errors.full_messages
-      },status: :bad_request
-    end
     
+    if params[:tags].present?
+      tags = params[:tags].map do |tag_name|
+        Tag.find_or_create_by(name: tag_name.strip)
+      end
+      spot.tags = tags # 関連付けを更新
+    end
+
+    
+    if spot.update(spot_params)
+      render json: { spot: spot.as_json(include: { tags: { only: [:id, :name] } }) }, status: :ok
+    else
+      render json: { errors: spot.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
