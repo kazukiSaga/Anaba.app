651c6d84368258605b6fc58de033557c81195747
kazukiSaga <kazukikk1351@gmail.com>
2024-12-20 01:44:37 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
-  # 以下を追加
   def test
     render json: { message: 'This is a test message from Rails' }
   end
