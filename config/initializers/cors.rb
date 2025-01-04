ddcd83681f0652ca17a6e11a25847ff7a6e0463e
kazukiSaga <kazukikk1351@gmail.com>
2025-01-04 13:48:57 +0900

    cros.rb修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:5173', 'https://anaba.onrender.com'
+    origins 'localhost:5173', 'https://www.anabashare.com'
 
     resource '*',
       headers: :any,
