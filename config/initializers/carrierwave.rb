c0d6709101f872aa9665036ab4bf88a6aed9ca4e
kazukiSaga <kazukikk1351@gmail.com>
2025-02-02 16:22:35 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,16 +5,22 @@
 #     config.asset_host = 'http://localhost:3000'
 #   end
 # end
+
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.asset_host = 'https://anaba-front.vercel.app'
     config.storage = :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'anaba-bucket'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
       aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-      region:                Rails.application.credentials.dig(:aws, :region)
+      region:                Rails.application.credentials.dig(:aws, :region),
+      path_style: true
     }
     config.fog_directory = Rails.application.credentials.dig(:aws, :bucket)
     config.fog_public = true # 公開設定を変更可能
