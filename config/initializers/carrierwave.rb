66a634cb6e1662399de6122ac6f4e6daf6dd1258
kazukiSaga <kazukikk1351@gmail.com>
2025-02-02 16:36:35 +0900

    update

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,6 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'anaba-bucket'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
