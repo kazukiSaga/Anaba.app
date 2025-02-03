0e565433c1c1b6c2dcc35f3b04863f29ff522f08
kazukiSaga <kazukikk1351@gmail.com>
2025-02-03 12:50:30 +0900

    コメントアウト削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_api_v1_user!
 
-  
+
   def index
      render json: {
       user: current_api_v1_user
@@ -28,7 +28,6 @@ class UsersController < ApplicationController
       render json: { message: "ニックネームが更新されました", name: current_api_v1_user.name }, status: :ok
     else
       render json: { error: "ニックネームの更新に失敗しました" }, status: :unprocessable_entity
-      # render json: { error: current_api_v1_user.errors.full_messages.join(", ") }, status: :unprocessable_entity
     end
   end
 
