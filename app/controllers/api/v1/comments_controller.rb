234ef0fc569f341638f49fb9ba2e920746dfcfd1
kazukiSaga <kazukikk1351@gmail.com>
2025-07-26 15:43:24 +0900

    未ログイン時にコメント閲覧できるよう修正

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CommentsController < ApplicationController
-  before_action :authenticate_api_v1_user!
+  before_action :authenticate_api_v1_user!, except: [:index, :show]
 
 
   def index
